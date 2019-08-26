26b6cbd79989a22b7fec6d76cd7cae94f987b9b9
jofu1123 <fantasyoprd@gmail.com>
2019-08-26 20:25:57 +0800

    1.精簡app.js部分程式碼 2.處理瀏覽器找不到favion.ico檔案錯誤訊息

--- app.js
+++ app.js
@@ -39,9 +39,9 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 
 // routes setting search function 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) | restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  const keyword = req.query.keyword.toLowerCase()
+  const restaurants = restaurantList.results.filter(({ name, category }) => {
+    return name.toLowerCase().includes(keyword) || category.toLowerCase().includes(keyword)
   })
 
   res.render('index', { restaurants, keyword })
