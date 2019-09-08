cabb1bc7174fafdd4cf760928e0dbf93c99df90a
jofu1123 <fantasyop1123@gmail.com>
2019-09-08 17:42:47 +0800

    Add sort feature, add delete alert

--- routes/home.js
+++ routes/home.js
@@ -3,10 +3,16 @@ const router = express.Router()
 const Restaurant = require('../models/list')
 
 router.get('/', (req, res) => {
+  const sort = req.query.sort || 'asc'
+  const sortName = req.query.sortName || 'name'
+  const sortObject = {}
+  sortObject[sortName] = sort
+
   Restaurant.find((err, list) => {
     if (err) return console.error(err)
-    return res.render('index', { restaurants: list })
+    return res.render('index', { restaurants: list, sortName, sort })
   })
+
 })
 
 router.get('/restaurants', (req, res) => {
--- routes/search.js
+++ routes/search.js
@@ -4,13 +4,21 @@ const Restaurant = require('../models/list')
 
 // routes setting search function 
 router.get('/', (req, res) => {
-  Restaurant.find((err, allRestaurantList) => {
-    const keyword = req.query.keyword.toLowerCase()
-    const restaurants = allRestaurantList.filter(({ name, category }) => {
-      return name.toLowerCase().includes(keyword) || category.toLowerCase().includes(keyword)
-    })
-    res.render('index', { restaurants, keyword })
-  })
-})
+  const sort = req.query.sort || 'asc'
+  const sortName = req.query.sortName || 'name'
+  const sortObject = {}
+  sortObject[sortName] = sort
 
+  Restaurant.find()
+    .sort(sortObject)
+    .exec((err, allRestaurantList) => {
+      const keyword = req.query.keyword.toLowerCase()
+      const restaurants = allRestaurantList.filter(({ name, category }) => {
+        return name.toLowerCase().includes(keyword) ||
+          category.toLowerCase().includes(keyword)
+      })
+      if (err) return console.error(err)
+      return res.render('index', { restaurants, sortName, sort, keyword })
+    })
+})
 module.exports = router
