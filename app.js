a05d5813d114747992ed4b1769b5dea2b797c714
jofu1123 <fantasyop1123@gmail.com>
2019-09-14 14:41:16 +0800

    Add isAuthenticated to display different view

--- app.js
+++ app.js
@@ -54,8 +54,11 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./config/passport')(passport)
+
+//登入後可以取得使用者的資訊方便我們在view中直接使用
 app.use((req, res, next) => {
   res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated() //辨識使用者是否已經登入
   next()
 })
 
