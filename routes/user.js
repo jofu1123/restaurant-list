419b83a74f507e6f738c908833288e7fd4a422d5
jofu1123 <fantasyop1123@gmail.com>
2019-09-15 13:55:27 +0800

    Add bcrypt to register and login check route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 
 // 登入頁面
@@ -38,9 +39,18 @@ router.post('/register', (req, res) => {
         email,
         password
       })
-      newUser.save().then(
-        res.redirect('/users/login')
-      ).catch(err => console.log(err))
+
+      //use bcrypt
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+
+          newUser.save().then(
+            res.redirect('/users/login')
+          ).catch(err => console.log(err))
+        })
+      })
     }
   })
 })
